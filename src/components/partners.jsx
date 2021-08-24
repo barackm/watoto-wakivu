31de6ecce5057dff5da3a7bfaec686fc40cfabb0
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-24 10:18:22 +0200

    Add partners logos

--- src/components/partners.jsx
+++ src/components/partners.jsx
@@ -1,4 +1,10 @@
 import React, { Component } from "react";
+import partner1 from '../assets/images/partners/partner1.jpeg'
+import partner2 from '../assets/images/partners/partner2.jpeg'
+import partner3 from '../assets/images/partners/partner3.jpeg'
+import partner4 from '../assets/images/partners/partner4.jpeg'
+import partner5 from '../assets/images/partners/partner5.jpeg'
+import partner6 from '../assets/images/partners/partner6.jpeg'
 
 class Partners extends Component {
   state = {};
@@ -16,10 +22,24 @@ class Partners extends Component {
         </div>
         <div className="partners-list-main-container">
           <div className="partners-grid">
-            <div className="partner-item"></div>
-            <div className="partner-item"></div>
-            <div className="partner-item"></div>
-            <div className="partner-item"></div>
+            <div className="partner-item">
+              <img src={partner2} alt="partner" />
+            </div>
+            <div className="partner-item">
+              <img src={partner1} alt="partner" />
+            </div>
+            <div className="partner-item">
+              <img src={partner3} alt="partner" />
+            </div>
+            <div className="partner-item">
+            <img src={partner6} alt="partner" />
+            </div>
+            <div className="partner-item">
+            <img src={partner4} alt="partner" />
+            </div>
+            <div className="partner-item">
+            <img src={partner5} alt="partner" />
+            </div>
           </div>
         </div>
       </div>
